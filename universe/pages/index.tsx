8e5b0fc201087d450e8327c9da10d692d9aaec76
AndruRachi <andruvrch@gmail.com>
2023-05-08 17:06:35 -0500

    Creación de la primera parte del home pre-login

--- universe/pages/index.tsx
+++ universe/pages/index.tsx
@@ -1,29 +1,35 @@
 
 import { Inter } from 'next/font/google'
-import LateralNavBar from '../Component/LateralNavBar'
 import Navbar from '../Component/NavBar'
 import Head from 'next/head';
+import style from "/styles/homeStyle.module.css";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Universe</title>
-      </Head>
-      <header>
-        <Navbar></Navbar>
-        <LateralNavBar></LateralNavBar>
-        {/*Agrego los componentes dentro del header*/}
+      <>
+          <div className={style.Home}>
 
-      </header>
+              <Head>
+                <title>Universe</title>
+              </Head>
+              <header>
+                <Navbar></Navbar>
+                {/*Agrego los componentes dentro del header*/}
 
-      <main>
+              </header>
+                <div className={style.imagen_logo}>
+                    <img src="/images/universelogo.png" alt='logo'/>
+                </div>
 
-        <h1>Pruebas de posicionamiento</h1>
-      </main>
-    </>
+                <div className={style.rectangle}>
+                  <h1 className={style.rectangle_text}>¿Sobre qué quieres aprender hoy?</h1>
+                </div>
+
+          </div>
+
+      </>
 
   )
 }
