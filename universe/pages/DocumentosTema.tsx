f143c7c64ccf6d9402dc3e158c79b2b58d7f88b3
juandp1 <jupalaciosf@unal.edu.co>
2023-06-04 17:05:47 -0500

    creacion del formulario para crear documentos

--- universe/pages/DocumentosTema.tsx
+++ universe/pages/DocumentosTema.tsx
@@ -1,32 +1,91 @@
 import Head from "next/head";
+import style from "/styles/DocumentosTema.module.css";
 import * as Faicon from 'react-icons/fa';
 import * as IoIcon from 'react-icons/io';
 import * as HiIcon from 'react-icons/hi';
 import LateralNavBar from "universe/Component/LateralNavBar";
 import Navbar from "universe/Component/NavBar";
-import { useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { Formik } from 'formik';
 import TarjetaDocumento from "universe/Component/TarjetaDocumento";
+import { ReactSVG } from 'react-svg';
+import Select from 'react-select';
+
+
+
+import { type } from "os";
 
 
 
 const colorIcon = "#61EB8D"
 interface Documento {
-    nombre_Documento: String;
+    name: String;
+    description: String;
+    file: String;
+    type: String;
+}
+interface Options {
+    value: string;
+    text: string;
 }
 
+const optionsType: Options[] = [
+    {
+        value: "libro",
+        text: "Libro"
+    },
+    {
+        value: "examen",
+        text: "Examen"
+    },
+    {
+        value: "ejercicio",
+        text: "Ejercicio"
+    }
+]
+
+
+
 export default function DocumentosTema() {
+    const [isAdmin, setIsAdmin] = useState(false)
     const [showFormAñadirDocumento, setShowFormAñadirDocumento] = useState(false)
     const statusShowFormAñadirDocumento = () => setShowFormAñadirDocumento(!showFormAñadirDocumento)
+    const [file, setFile] = useState<File | null>(null);
     const [Documentos, setDocumentos] = useState([{
         nombreDocumentos: ''
     }])
+    const [optionType, setOptionType] = useState<string | null>("libro");
 
     const toggle = () => {
         var blurMain = document.getElementById("main")
         blurMain?.classList.toggle("active")
         statusShowFormAñadirDocumento()
     }
+    // FUNCION PARA SABER SI ES ADMIN( PUEDE QUE LO MEJOR SEA MOVERLA A HOME COMUNIDAD)
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await fetch('/api/is_admin', {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        "community_id": localStorage.getItem("comunidad_ID"),
+                        "used_id": localStorage.getItem("used_ID")
+                    })
+                });
+
+                if (res.status == 200) {
+                    setIsAdmin(true)
+                }
+            } catch (error: any) {
+                console.error('Error:', error);
+                alert(error.message);
+            }
+        }
+        fetchData();
+    }, []);
     const GetInfoTemas = async (values: Documento) => { // se trae la informacion de los documentos que existen al entrar a la pagina
         //setIsLoading(true)
         let url: string = 'https://decorisaserver.azurewebsites.net/api/cita/key/'//+ nombreComunidad 
@@ -42,10 +101,71 @@ export default function DocumentosTema() {
             })
 
     }
-    const añadirDocumento = async (values: Documento) => {
+    const crearDocumento = async (values: Documento) => {
         /**funcion para añadir un documento y llevarlo al backend */
+        
+
+        if (file != null) {
+
+            try {
+                const res = await fetch('http://127.0.0.1:3333/api/document', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ "name": values.name, "description": values.description, "file": file, "type": optionType })
+                })
+                    .then(res => res.json())
+                    .catch(error => console.error('Error:', error))
+                    .then(response => console.log('Success:', response))
+            } catch (error: any) {
+                console.error('Error:', error);
+                alert(error.message);
+            }
+
+        } else {
+            console.error('Error:', "no ha ingresado ningun archivo");
+        }
+
+
 
     }
+    const proponerDocumento = async (values: Documento) => {
+        
+
+        if (file != null) {
+
+            try {
+                const res = await fetch('http://127.0.0.1:3333/api/document', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ "name": values.name, "description": values.description, "file": file, "type": optionType })
+                })
+                    .then(res => res.json())
+                    .catch(error => console.error('Error:', error))
+                    .then(response => console.log('Success:', response))
+            } catch (error: any) {
+                console.error('Error:', error);
+                alert(error.message);
+            }
+
+        } else {
+            console.error('Error:', "no ha ingresado ningun archivo");
+        }
+
+
+
+    }
+
+    const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const fileList = event.target.files;
+        if (fileList) {
+            setFile(fileList[0]);
+        }
+        console.log(file)
+    };
 
     return (
         <>
@@ -59,7 +179,7 @@ export default function DocumentosTema() {
                 <div className="principal_Content">
 
                     <div className="flex items-center justify-start space-x-3">
-                        <HiIcon.HiDocumentText size={"100px"} color={colorIcon} />
+                        <HiIcon.HiDocumentText size={"80px"} color={colorIcon} />
                         <div style={{ display: 'flex', flexDirection: 'column' }}>
                             <h2 style={{ alignSelf: 'flex-start' }}>Documentos</h2>
                             <h5 style={{ alignSelf: 'flex-start' }}>Selecciona los documentos que quieras ver del tema que seleccionaste</h5>
@@ -81,12 +201,12 @@ export default function DocumentosTema() {
                         <TarjetaDocumento idDocument={7} DocumentName="Ejercicio pre-parcial" descripcion="Descripcion del documento" docType={"ejercicio"}></TarjetaDocumento>
                     </div>
                     <div className="corner_Content">
-                    <div className="flex items-center justify-end space-x-3">
-                        
-                        <h3>Añadir nuevo documento</h3>
-                        <div className="button_crear" onClick={toggle}>
-                            <HiIcon.HiOutlineDocumentAdd size={'60px'} color={colorIcon} />
-                        </div>
+                        <div className="flex items-center justify-end space-x-3">
+
+
+                            <div className="button_crear" onClick={toggle}>
+                                <HiIcon.HiOutlineDocumentAdd size={'60px'} color={colorIcon} />
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -98,7 +218,105 @@ export default function DocumentosTema() {
 
             {showFormAñadirDocumento ? (
                 <div>
+                    <Formik
+                        initialValues={{
+                            name: "",
+                            description: "",
+                            file: "",
+                            type: "",
+                        }}
+                        onSubmit={async (values) => {
 
+                            isAdmin?(crearDocumento(values)):(proponerDocumento(values))
+                            
+                            //alert(JSON.stringify(values));
+                        }}
+                    >
+                        {({ handleSubmit, values, handleChange }) => (
+                            <form id="login" onSubmit={handleSubmit}>
+                                <div id="encabezado">
+                                    <IoIcon.IoMdClose size={"25px"} onClick={toggle} id="close" />
+
+                                    <div>
+
+                                        <HiIcon.HiDocumentText size={"60px"} color={"#1D3752"} />
+                                        {isAdmin ? (
+                                            (
+                                                <h2>Añadir Documento</h2>
+
+                                            )
+                                        ) : (
+                                            <h2>Proponer Documento</h2>
+
+                                        )}
+
+                                    </div>
+                                    <div>
+                                        {isAdmin ? (
+                                            (
+                                                <button type="submit">
+                                                    <h3>Añadir</h3>
+                                                </button>
+
+                                            )
+                                        ) : (
+                                            <button type="submit">
+                                                <h3>Proponer</h3>
+                                            </button>
+
+                                        )}
+
+                                    </div>
+                                </div>
+                                <div id="inputs">
+                                    <div>
+                                        <h5>Titulo:</h5>
+                                        <input name="name" type="text" placeholder="Titulo"
+                                            value={values.name}
+                                            onChange={handleChange}
+                                        />
+                                        <h5>Descripcion:</h5>
+                                        <input name="description" type="text" placeholder="Descripcion"
+                                            value={values.description}
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+                                    {/**segunda columna del formulario esi es necesario */}
+                                    <div>
+                                        <h5>Archivo:</h5>
+                                        <input name="file" type="file"
+                                            //value={values.file}
+                                            onChange={handleFileChange}
+                                            placeholder="Subir"
+                                            content="Subir"
+                                        />
+
+
+
+                                        <h5>Tipo de Archivo:</h5>
+                                        <div className={style.SelectType}>
+                                            <Select
+                                                // If you don't need a state you can remove the two following lines value & onChange
+
+                                                placeholder="Tipo"
+                                                onChange={(option: Options | null) => {
+                                                    if (option != null) {
+                                                        setOptionType(option.value);
+                                                    }
+                                                }}
+                                                getOptionLabel={(option: Options) => option.text}
+                                                getOptionValue={(option: Options) => option.value}
+                                                options={optionsType}
+                                                isSearchable={false}
+
+                                            />
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </form>
+                        )}
+                    </Formik>
                 </div>
             ) : null}
         </>
