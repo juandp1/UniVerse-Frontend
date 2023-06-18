1a1f2ba10676a50384fb5f523d7a26d2a9a052e4
AndruRachi <andruvrch@gmail.com>
2023-06-17 22:06:53 -0500

    Test generales en reuniones

--- universe/pages/ProximasReuniones.tsx
+++ universe/pages/ProximasReuniones.tsx
@@ -4,186 +4,194 @@ import Head from "next/head";
 import Reunion from "universe/Component/Reunion";
 import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import Cookies from "js-cookie";
+import { GetServerSideProps } from "next/types";
+import nookies from "nookies";
+import { Formik } from "formik";
+import style from "/styles/ReunionesStyle.module.css";
+import * as IoIcon from "react-icons/io";
+import * as HiIcon from "react-icons/hi";
+import ConfirmacionRecuadro from "universe/Component/ConfirmacionRecuadro";
+import { number } from "yup";
 
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  context.res.setHeader("Cache-Control", "no-store, must-revalidate");
+  const token = nookies.get(context).token;
 
+  if (!token) {
+    //Si no esta logeado lo redirige al Login
+    return {
+      redirect: {
+        destination: "/Login",
+        permanent: false,
+      },
+    };
+  }
 
+  //Si esta logeado le muestra la pagina
+  return {
+    props: {}, // Muestra la pagina
+  };
+};
 
 interface Reunion {
-  id: number;
-  nombreReu: string;
-  descripcion_reu: string;
-  fecha_reu: string;
-  hora_reu: string;
-  lugar_reu: string;
+  nombreReunion: string;
+  descripcion_reunion: string;
+  fecha_reunion: string;
+  hora_reunion: string;
+  lugar_reunion: string;
 }
 
 const colorIcon = "#61EB8D";
-var idReunion: number
-var nombreCreador: string;
-var nombreReunion: string;
-var descripcion_reunion: string;
-var fecha_reunion: string;
-var hora_reunion: string;
-var lugar_reunion: string;
-
-
-
+var id_reunion: number;
 
 export default function ProximasReuniones() {
+  //VARIABLES USE STATE
 
-  const [Reuniones, setReuniones] = useState([{
-    idReunion: 0,
-    nombreReunion: "",
-    descripcion_reunion: "",
-    fecha_reunion: "",
-    hora_reunion: "",
-    lugar_reunion: "",
-  }])
+  const [confirmacion, setConfirmacion] = useState(false);
+  const stateConfirmacion = () => {
+    setConfirmacion(!confirmacion);
+    toggle();
+  };
+  const [formEditar, setformEditar] = useState(false);
+  const stateformEditar = () => setformEditar(!formEditar);
 
-    //VARIABLES USE STATE
+  const [Community_id, setCommunity_id] = useState<string | null>("");
+  useEffect(() => {
+    setCommunity_id(localStorage.getItem("comunidad_ID"));
+  }, []);
 
-    const [confirmacion, setConfirmacion] = useState(false)
-    const stateConfirmacion = () => {
-        setConfirmacion(!confirmacion)
-        toggle()
-    }
-    const [formEditar, setformEditar] = useState(false)
-    const stateformEditar = () => setformEditar(!formEditar)
+  const [Reuniones, setReuniones] = useState([
+    {
+      id: 0,
+      name: "",
+      description: "",
+      place: "",
+      date: "",
+      community_id: 0,
+      author_id: 0,
+    },
+  ]);
 
-
-    
-    //FUNCIÓN PARA TRAER TODAS LAS PRÓXIMAS REUNIONES APENAS CARGA LA PÁGINA
-    useEffect(() => {
-      const fetchData = async () => { // se trae la información de las reuniones que existen al entrar a la página.
-          try {
-              const res = await fetch('http://localhost:3333/api/meetings/community/' + localStorage.getItem("comunidad_ID"), {
-                  method: 'GET',
-                  headers: {
-                      'Content-Type': 'application/json'
-                  },
-                  body: JSON.stringify({'community_ID': localStorage.getItem("comunidad_ID"), 'initial_date': Date.now() , 'final_date': Date.now()+30})
-              });
-              if (res.ok) {
-                  const data = await res.json();
-                  setReuniones(data)
-              }
-          } catch (error: any) {
-              console.error('Error:', error);
-              alert(error.message);
+  //FUNCIÓN PARA TRAER TODAS LAS PRÓXIMAS REUNIONES APENAS CARGA LA PÁGINA
+  useEffect(() => {
+    const fetchData = async () => {
+      // se trae la información de las reuniones que existen al entrar a la página.
+      console.log(Date.now())
+      console.log(Date.now()+30)
+      try {
+        let finalDate = new Date();
+        finalDate.setMonth(finalDate.getMonth() + 1)
+        console.log(finalDate)
+        const res = await fetch(
+          "http://localhost:3333/api/meetings/community/" + localStorage.getItem("comunidad_ID"),
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${localStorage.getItem("token")}`
+            },
+            body: JSON.stringify({
+              community_ID: localStorage.getItem("comunidad_ID"),
+              initial_date: Date.now(),
+              final_date: finalDate,
+            }),
           }
-
+        );
+        if (res.ok) {
+          const data = await res.json();
+          console.log(data)
+          setReuniones(data['meetings']);
+        }
+      } catch (error: any) {
+        console.error("Error:", error);
+        alert(error.message);
       }
-      fetchData();
-    }, []);
+    };
+    fetchData();
+  }, []);
 
+  //EDICION DE LA COMUNIDAD
+  const editarReunion = (id: number) => {
+    id_reunion = id;
+    stateformEditar();
+    toggle();
+  };
 
-    // OBTENCION DE TODAS LAS COMUNIDADES 
-    useEffect(() => {
-        const fetchData = async () => { // se trae la informacion de los documentos que existen al entrar a la pagina
-            //setIsLoading(true)
-            try {
-                const res = await fetch("http://localhost:3333/api/meetings", {
-                    method: 'GET',
-                    mode: 'cors',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${Cookies.get('token')}`
-                    }
-                });
-                if (res.ok) {
-                    const data = await res.json();
-                    setReuniones(data.meetings)
-                }
-            } catch (error: any) {
-                console.error('Error:', error);
-                alert(error.message);
-            }
+  const cerrarEdicion = () => {
+    stateformEditar();
+    toggle();
+  };
+
+  //UPDATE REUNION
+  const updateReunion = async (values: Reunion) => {
+    const dateTime =
+      values.fecha_reunion + " " + values.hora_reunion + ":00.000000";
+    try {
+      const res = await fetch(
+        "http://localhost:3333/api/meetings/community/" + Community_id,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${Cookies.get("token")}`,
+          },
+          body: JSON.stringify({
+            name: values.nombreReunion,
+            description: values.descripcion_reunion,
+            place: values.lugar_reunion,
+            date: dateTime,
+          }),
         }
-        fetchData();
-    }, []);
+      );
 
-
-
-
-    //EDICION DE LA COMUNIDAD
-    const editar = (id: number,  nombreReu: String, descripcion_reu: String, fecha_reu: String, hora_reu: String, lugar_reu: String) => {
-      idReunion =   id
-      nombreReunion = nombreReu
-      descripcion_reunion = descripcion_reu
-      fecha_reunion = fecha_reu
-      hora_reunion = hora_reu
-      lugar_reunion =  lugar_reu
-        stateformEditar()
-        toggle()
+      if (res.ok) {
+      } else {
+        throw new Error("ha sucedido un error al editar la reunión");
+      }
+    } catch (error: any) {
+      console.error("Error:", error);
+      alert(error.message);
     }
-
-    const cerrarEdicion = () => {
-        stateformEditar()
-        toggle()
-    }
-
-    //UPDATE REUNION
-    const updateReunion = async (values: Comunidad) => {
-        try {
-            const res = await fetch('http://localhost:3333/api/community/name/' + comunityName, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${Cookies.get('token')}`
-                },
-                body: JSON.stringify({ "name": values.nameComunidad, "description": values.descripcion, "label": values.materia })
-            });
-
-            if (res.ok) {
-
-            } else {
-                throw new Error('ha sucedido un error al crear la comunidad');
-            }
-        } catch (error: any) {
-            console.error('Error:', error);
-            alert(error.message);
+    stateformEditar();
+    toggle();
+  };
+  const eliminarReunion = (reunion_ID: number) => {
+    id_reunion = reunion_ID;
+    stateConfirmacion();
+  };
+  const deleteReunion = async () => {
+    try {
+      const res = await fetch(
+        "http://localhost:3333/api/meeting/id/" + id_reunion,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${Cookies.get("token")}`,
+          },
         }
-        stateformEditar()
-        toggle()
+      );
+
+      if (res.ok) {
+        console.log("Error:", "Se ha eliminado la reunión de forma correcta");
+        alert("Se ha eliminado la reunión de forma correcta");
+        stateConfirmacion();
+      } else {
+        throw new Error("ha sucedido un error al elimianr la reunión");
+      }
+    } catch (error: any) {
+      console.error("Error:", error);
+      alert(error.message);
     }
-    const eliminar = (comunnuty_ID: number, name: string) => {
-        id_community = comunnuty_ID
-        comunityName = name
-        stateConfirmacion()
-    }
-    const deleteReunion = async () => {
-        try {
-            const res = await fetch('http://localhost:3333/api/community/name/' + comunityName, {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${Cookies.get('token')}`
-                }
+    stateformEditar();
+    toggle();
+  };
 
-            });
-
-            if (res.ok) {
-                console.log('Error:', "Se ha eliminado la comunidad de forma correcta");
-                alert("Se ha eliminado la comunidad de forma correcta");
-            } else {
-                throw new Error('ha sucedido un error al elimianr la comunidad');
-            }
-        } catch (error: any) {
-            console.error('Error:', error);
-            alert(error.message);
-        }
-        stateformEditar()
-        toggle()
-    }
-
-    // FUNCION TOGGLE  se encarga de desvanecer el fondo cuando se despliega un formulario
-    const toggle = () => {
-        var blurMain = document.getElementById("main")
-        blurMain?.classList.toggle("active")
-    }
-
-
-
+  // FUNCION TOGGLE  se encarga de desvanecer el fondo cuando se despliega un formulario
+  const toggle = () => {
+    var blurMain = document.getElementById("main");
+    blurMain?.classList.toggle("active");
+  };
 
   return (
     <>
@@ -211,15 +219,128 @@ export default function ProximasReuniones() {
             <h1>Próximas Reuniones</h1>
           </div>
           <div>
-            <Reunion idReunion={1} nombreCreador={"Creador #1"} nombreReunion={"Reunion número uno"} descripcion_reunion={"Esta es la reunión número uno"} fecha_reunion={"2023/11/29"} hora_reunion={"10:50pm"} lugar_reunion={"CyT"} editar={editar} eliminar={eliminar}></Reunion>
-            <Reunion idReunion={2} nombreCreador={"Creador #2"} nombreReunion={"Reunion número dos"} descripcion_reunion={"Esta es la reunión número dos"} fecha_reunion={"2023/11/29"} hora_reunion={"10:50pm"} lugar_reunion={"CyT"}></Reunion>
-            <Reunion idReunion={3} nombreCreador={"Creador #3"} nombreReunion={"Reunion número tres"} descripcion_reunion={"Esta es la reunión número tres"} fecha_reunion={"2023/11/29"} hora_reunion={"10:50pm"} lugar_reunion={"CyT"}></Reunion>
-            <Reunion idReunion={4} nombreCreador={"Creador #4"} nombreReunion={"Reunion número cuatro"} descripcion_reunion={"Esta es la reunión número cuatro"} fecha_reunion={"2023/11/29"} hora_reunion={"10:50pm"} lugar_reunion={"CyT"}></Reunion>
-            <Reunion idReunion={5} nombreCreador={"Creador #5"} nombreReunion={"Reunion número cinco"} descripcion_reunion={"Esta es la reunión número cinco"} fecha_reunion={"2023/11/29"} hora_reunion={"10:50pm"} lugar_reunion={"CyT"}></Reunion>
+            {Reuniones.map((item, index) => {
+              return (
+                <Reunion
+                  key={index}
+                  idReunion={item.id}
+                  nombreReunion={item.name}
+                  descripcion_reunion={item.description}
+                  lugar_reunion={item.place}
+                  fecha_reunion={item.date}
+                  editar={editarReunion}
+                  eliminar={eliminarReunion}
+                ></Reunion>
+              );
+            })}
           </div>
         </div>
         {/*Agrego los componentes dentro del header*/}
       </main>
+
+      {confirmacion ? (
+        <div className="modalOverlay">
+          <ConfirmacionRecuadro
+            name={"Reunión"}
+            eliminar={deleteReunion}
+            cerrar={stateConfirmacion}
+          ></ConfirmacionRecuadro>
+        </div>
+      ) : null}
+
+      {formEditar ? (
+        <div>
+          <Formik
+            initialValues={{
+              nombreReunion: "",
+              descripcion_reunion: "",
+              fecha_reunion: "",
+              hora_reunion: "",
+              lugar_reunion: "",
+            }}
+            onSubmit={async (values) => {
+              updateReunion(values);
+              //alert(JSON.stringify(values));
+            }}
+          >
+            {({ handleSubmit, values, handleChange }) => (
+              <form onSubmit={handleSubmit}>
+                <div id="encabezado">
+                  <IoIcon.IoMdClose size={"25px"} onClick={toggle} id="close" />
+
+                  <div>
+                    <HiIcon.HiFolderAdd size={"60px"} color={"#1D3752"} />
+                    <h2>Crear una nueva reunión</h2>
+                  </div>
+                  <div>
+                    <button type="submit">
+                      <h3>Crear</h3>
+                    </button>
+                  </div>
+                </div>
+
+                <div id="inputs">
+                  <div>
+                    <h5>Nombre de la reunión:</h5>
+                    <input
+                      name="nombreReunion"
+                      type="text"
+                      placeholder="Nombre Reunion"
+                      value={values.nombreReunion}
+                      onChange={handleChange}
+                    />
+
+                    <h5>Descripción de la reunión:</h5>
+                    <input
+                      name="descripcion_reunion"
+                      type="text"
+                      placeholder="descripcion reunion"
+                      value={values.descripcion_reunion}
+                      onChange={handleChange}
+                    />
+                    <h4 className="my-5">Detalles del encuentro</h4>
+
+                    <div className={style.Detalles_Reunion}>
+                      <div>
+                        <input
+                          id="Detalles_Encuentro"
+                          name="fecha_reunion"
+                          type="date"
+                          placeholder="Fecha reunion"
+                          value={values.fecha_reunion}
+                          onChange={handleChange}
+                        />
+                        <label>Fecha de la reunión:</label>
+                      </div>
+                      <div>
+                        <input
+                          name="hora_reunion"
+                          type="time"
+                          placeholder="Hora reunion"
+                          value={values.hora_reunion}
+                          onChange={handleChange}
+                        />
+                        <label>Hora de la reunión:</label>
+                      </div>
+
+                      <div>
+                        <input
+                          name="lugar_reunion"
+                          type="text"
+                          placeholder="Lugar reunion"
+                          value={values.lugar_reunion}
+                          onChange={handleChange}
+                        />
+                        <label>Lugar de la reunión:</label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            )}
+          </Formik>
+        </div>
+      ) : null}
     </>
   );
 }
