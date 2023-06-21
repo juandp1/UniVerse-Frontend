312c85726d7f3474619bd3bed2c28f581f367075
Carlos <csandovalc@unal.edu.co>
2023-06-20 19:28:32 -0500

    feat(documents): add endpoint to get document by topic and community

--- universe/pages/DocumentosTema.tsx
+++ universe/pages/DocumentosTema.tsx
@@ -50,8 +50,8 @@ const optionsType: Options[] = [
 		text: "Ejercicio",
 	},
 ];
-var document_id:number
-var document_name:string
+var document_id: number;
+var document_name: string;
 
 export default function DocumentosTema() {
 	const [Topic_id, setTopic_id] = useState<string | null>("");
@@ -60,32 +60,30 @@ export default function DocumentosTema() {
 	const [showFormAñadirDocumento, setShowFormAñadirDocumento] = useState(false);
 	const statusShowFormAñadirDocumento = () => {
 		setShowFormAñadirDocumento(!showFormAñadirDocumento);
-		toggle()
-	}
-
+		toggle();
+	};
 
 	const [file, setFile] = useState<File | null>(null);
 	const [Documentos, setDocumentos] = useState<DocumentResponse[]>([]);
 	const [optionType, setOptionType] = useState<string | null>("libro");
-	const [confirmacionEliminacion, setConfirmacionEliminacion] = useState(false)
+	const [confirmacionEliminacion, setConfirmacionEliminacion] = useState(false);
 	const [actualizacion, setActualizacion] = useState(0);
 	const newActualizacion = () => {
 		setActualizacion(actualizacion + 1);
 	};
 	const stateConfirmacionEliminacion = () => {
-		setConfirmacionEliminacion(!confirmacionEliminacion)
-		toggle()
-	}
+		setConfirmacionEliminacion(!confirmacionEliminacion);
+		toggle();
+	};
 	const toggle = () => {
 		var blurMain = document.getElementById("main");
 		blurMain?.classList.toggle("active");
-		
 	};
-	const eliminarDocumento = (id: number,name_documento: string) => {
-        document_id = id
-        document_name = name_documento
-        stateConfirmacionEliminacion()
-    }
+	const eliminarDocumento = (id: number, name_documento: string) => {
+		document_id = id;
+		document_name = name_documento;
+		stateConfirmacionEliminacion();
+	};
 
 	useEffect(() => {
 		setTopic_id(localStorage.getItem("Topic"));
@@ -104,7 +102,9 @@ export default function DocumentosTema() {
 			//setIsLoading(true)
 			try {
 				const res = await fetch(
-					"https://universe-backend.azurewebsites.net/api/topic/" + (localStorage.getItem("Topic") ?? "0") + "/documents",
+					`https://universe-backend.azurewebsites.net/api/community/${localStorage.getItem(
+						"comunidad_ID"
+					)}/documents/${localStorage.getItem("Topic")}`,
 					{
 						method: "GET",
 						headers: {
@@ -121,7 +121,7 @@ export default function DocumentosTema() {
 					console.log(await res.json());
 				}
 			} catch (error: any) {
-				console.error("Error:", error);
+				console.error("Error:", String(error));
 				alert(error.message);
 			}
 		};
@@ -131,40 +131,42 @@ export default function DocumentosTema() {
 	const deleteDocumento = async () => {
 		/**funcion para la creacion de un tema en el backend */
 		try {
-			const res = await fetch("https://universe-backend.azurewebsites.net/api/community/"+localStorage.getItem("comunidad_ID")+"/delete_document/"+document_id, {
-				method: "DELETE",
-				mode: "cors",
-				headers: {
-					"Content-Type": "application/json",
-					Authorization: `Bearer ${localStorage.getItem("token")}`,
+			const res = await fetch(
+				"https://universe-backend.azurewebsites.net/api/community/" +
+					localStorage.getItem("comunidad_ID") +
+					"/delete_document/" +
+					document_id,
+				{
+					method: "DELETE",
+					mode: "cors",
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: `Bearer ${localStorage.getItem("token")}`,
+					},
 				}
-			});
+			);
 			if (res.ok) {
 				console.log("Success:", "se ha eliminado el documento de forma correcta ");
-				toast.success('El documento ha sido eliminado', {
-                    position: "top-right",
-                    autoClose: 5000,
-                    hideProgressBar: false,
-                    closeOnClick: true,
-                    pauseOnHover: true,
-                    draggable: true,
-                    progress: undefined,
-                    theme: "light",
-                    className: "toast_success_doc"
-
-                });
-                newActualizacion();
+				toast.success("El documento ha sido eliminado", {
+					position: "top-right",
+					autoClose: 5000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: true,
+					progress: undefined,
+					theme: "light",
+					className: "toast_success_doc",
+				});
+				newActualizacion();
 			} else {
-				console.log(res.json())
-				
+				console.log(res.json());
 			}
 		} catch (error: any) {
 			console.error("Error:", error);
 			alert(error.message);
 		}
-		stateConfirmacionEliminacion()
-
-		
+		stateConfirmacionEliminacion();
 	};
 
 	const crearDocumento = async (values: Documento) => {
@@ -177,21 +179,24 @@ export default function DocumentosTema() {
 		reader.readAsText(file);
 		reader.onload = async () => {
 			try {
-				const res = await fetch("https://universe-backend.azurewebsites.net/api/community/" + Community_id + "/documents", {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-						Authorization: `Bearer ${localStorage.getItem("token")}`,
-					},
-					body: JSON.stringify({
-						name: values.name,
-						description: values.description,
-						file: reader.result,
-						type: optionType,
-						topic_id: Topic_id,
-						administrator_id: null,
-					}),
-				});
+				const res = await fetch(
+					"https://universe-backend.azurewebsites.net/api/community/" + Community_id + "/documents",
+					{
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+							Authorization: `Bearer ${localStorage.getItem("token")}`,
+						},
+						body: JSON.stringify({
+							name: values.name,
+							description: values.description,
+							file: reader.result,
+							type: optionType,
+							topic_id: Topic_id,
+							administrator_id: null,
+						}),
+					}
+				);
 				if (res.ok) {
 					statusShowFormAñadirDocumento();
 					toast.success("El documento ha sido añadido correctamente", {
@@ -205,7 +210,6 @@ export default function DocumentosTema() {
 						theme: "light",
 						className: style.toast_success_doc,
 					});
-					
 				} else {
 					console.log(await res.json());
 				}
@@ -264,27 +268,27 @@ export default function DocumentosTema() {
 							DocumentName="Ejercicio 1.2"
 							descripcion="Descripcion del documento"
 							docType={"ejercicio"}
-						eliminar={eliminarDocumento}
+							eliminar={eliminarDocumento}
 						></TarjetaDocumento>
-					<TarjetaDocumento
-						idDocument={3}
-						DocumentName="Parcial 1"
-						descripcion="Descripcion del documento"
-						docType={"examen"}
-						eliminar={eliminarDocumento}
-					></TarjetaDocumento>
-				</div>
+						<TarjetaDocumento
+							idDocument={3}
+							DocumentName="Parcial 1"
+							descripcion="Descripcion del documento"
+							docType={"examen"}
+							eliminar={eliminarDocumento}
+						></TarjetaDocumento>
+					</div>
 
-				<div className="corner_Content">
-					<div className="flex items-center justify-end space-x-3">
-						<div className="button_crear" onClick={statusShowFormAñadirDocumento}>
-							<HiIcon.HiOutlineDocumentAdd size={"60px"} color={colorIcon} />
+					<div className="corner_Content">
+						<div className="flex items-center justify-end space-x-3">
+							<div className="button_crear" onClick={statusShowFormAñadirDocumento}>
+								<HiIcon.HiOutlineDocumentAdd size={"60px"} color={colorIcon} />
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-			{/**aqui empieza el formulario que aparecera sobre todo el contenido de la pagina en ese momento */}
-		</main >
+				{/**aqui empieza el formulario que aparecera sobre todo el contenido de la pagina en ese momento */}
+			</main>
 			<ToastContainer position="top-right" className={style.success_notification} />
 			{confirmacionEliminacion ? (
 				<ConfirmacionRecuadro
@@ -294,92 +298,90 @@ export default function DocumentosTema() {
 					cerrar={stateConfirmacionEliminacion}
 				></ConfirmacionRecuadro>
 			) : null}
-	{
-		showFormAñadirDocumento ? (
-			<div>
-				<Formik
-					initialValues={{
-						name: "",
-						description: "",
-						file: "",
-						type: "",
-					}}
-					onSubmit={async (values) => {
-						crearDocumento(values);
+			{showFormAñadirDocumento ? (
+				<div>
+					<Formik
+						initialValues={{
+							name: "",
+							description: "",
+							file: "",
+							type: "",
+						}}
+						onSubmit={async (values) => {
+							crearDocumento(values);
 
-						//alert(JSON.stringify(values));
-					}}
-				>
-					{({ handleSubmit, values, handleChange }) => (
-						<form id="login" onSubmit={handleSubmit}>
-							<div id="encabezado">
-								<IoIcon.IoMdClose size={"25px"} onClick={statusShowFormAñadirDocumento} id="close" />
+							//alert(JSON.stringify(values));
+						}}
+					>
+						{({ handleSubmit, values, handleChange }) => (
+							<form id="login" onSubmit={handleSubmit}>
+								<div id="encabezado">
+									<IoIcon.IoMdClose size={"25px"} onClick={statusShowFormAñadirDocumento} id="close" />
 
-								<div>
-									<HiIcon.HiDocumentText size={"60px"} color={"#1D3752"} />
-									{isAdmin ? <h2>Añadir Documento</h2> : <h2>Proponer Documento</h2>}
-								</div>
-								<div>
-									{isAdmin ? (
-										<button type="submit">
-											<h3>Añadir</h3>
-										</button>
-									) : (
-										<button type="submit">
-											<h3>Proponer</h3>
-										</button>
-									)}
-								</div>
-							</div>
-							<div id="inputs">
-								<div>
-									<h5>Titulo:</h5>
-									<input name="name" type="text" placeholder="Titulo" value={values.name} onChange={handleChange} />
-									<h5>Descripcion:</h5>
-									<input
-										name="description"
-										type="text"
-										placeholder="Descripcion"
-										value={values.description}
-										onChange={handleChange}
-									/>
-								</div>
-								{/**segunda columna del formulario esi es necesario */}
-								<div>
-									<h5>Archivo:</h5>
-									<input
-										name="file"
-										type="file"
-										//value={values.file}
-										onChange={handleFileChange}
-										placeholder="Subir"
-										content="Subir"
-									/>
-
-									<h5>Tipo de Archivo:</h5>
-									<div className={style.SelectType}>
-										<Select
-											// If you don't need a state you can remove the two following lines value & onChange
-											placeholder="Tipo"
-											onChange={(option: Options | null) => {
-												if (option != null) {
-													setOptionType(option.value);
-												}
-											}}
-											getOptionLabel={(option: Options) => option.text}
-											getOptionValue={(option: Options) => option.value}
-											options={optionsType}
-											isSearchable={false}
-										/>
+									<div>
+										<HiIcon.HiDocumentText size={"60px"} color={"#1D3752"} />
+										{isAdmin ? <h2>Añadir Documento</h2> : <h2>Proponer Documento</h2>}
+									</div>
+									<div>
+										{isAdmin ? (
+											<button type="submit">
+												<h3>Añadir</h3>
+											</button>
+										) : (
+											<button type="submit">
+												<h3>Proponer</h3>
+											</button>
+										)}
 									</div>
 								</div>
-							</div>
-						</form>
-					)}
-				</Formik>
-			</div>
-		) : null
-	}
+								<div id="inputs">
+									<div>
+										<h5>Titulo:</h5>
+										<input name="name" type="text" placeholder="Titulo" value={values.name} onChange={handleChange} />
+										<h5>Descripcion:</h5>
+										<input
+											name="description"
+											type="text"
+											placeholder="Descripcion"
+											value={values.description}
+											onChange={handleChange}
+										/>
+									</div>
+									{/**segunda columna del formulario esi es necesario */}
+									<div>
+										<h5>Archivo:</h5>
+										<input
+											name="file"
+											type="file"
+											//value={values.file}
+											onChange={handleFileChange}
+											placeholder="Subir"
+											content="Subir"
+										/>
+
+										<h5>Tipo de Archivo:</h5>
+										<div className={style.SelectType}>
+											<Select
+												// If you don't need a state you can remove the two following lines value & onChange
+												placeholder="Tipo"
+												onChange={(option: Options | null) => {
+													if (option != null) {
+														setOptionType(option.value);
+													}
+												}}
+												getOptionLabel={(option: Options) => option.text}
+												getOptionValue={(option: Options) => option.value}
+												options={optionsType}
+												isSearchable={false}
+											/>
+										</div>
+									</div>
+								</div>
+							</form>
+						)}
+					</Formik>
+				</div>
+			) : null}
 		</>
 	);
 }
